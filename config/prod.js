f24af68f9dc256d1d35bbe541e968bcdfdd7e693
TimKrauseUS <TimothyKrause@outlook.com>
2022-01-22 13:18:28 -0500

    updated production variables

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,11 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY,
+  twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  twitterBearerToken: process.env.TWITTER_BEARER_TOKEN,
+  gitHubClientID: process.env.GITHUB_CLIENT_ID,
+  gitHubClientSecret: process.env.GITHUB_CLIENT_SECRET,
   cookieKey: process.env.COOKIE_KEY,
   mongoURI: process.env.MONGO_URI,
 }
